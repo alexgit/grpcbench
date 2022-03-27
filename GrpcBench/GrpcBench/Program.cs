e5b71337d481741fcc4da039d8adcb8e8e7754d0
alexgit <alex.tashev@gmail.com>
2022-03-27 13:26:34 +0300

    initial commit

---
+++ GrpcBench/GrpcBench/Program.cs
@@ -0,0 +1,39 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Grpc.Net.Client;
+using GrpcService1;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcBench
+{
+    public class Program
+    {
+        GrpcChannel channel;
+        Greeter.GreeterClient client;
+
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Program>();
+        }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            channel = GrpcChannel.ForAddress("https://localhost:5001");
+            client = new Greeter.GreeterClient(channel);
+        }
+
+        [GlobalCleanup]
+        public void TearDown() 
+        {
+            channel.Dispose();
+        }
+
+        [Benchmark]
+        public async Task Bench()
+        {
+            await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+        }
+    }
+}
---
+++ GrpcBench/GrpcBench/bin/Release/net5.0/BenchmarkDotNet.Artifacts/results/GrpcBench.Program-report-github.md
@@ -0,0 +1,13 @@
+``` ini
+
+BenchmarkDotNet=v0.13.1, OS=Windows 10.0.22000
+Intel Core i7-7500U CPU 2.70GHz (Kaby Lake), 1 CPU, 4 logical and 2 physical cores
+.NET SDK=6.0.100
+  [Host]     : .NET 5.0.13 (5.0.1321.56516), X64 RyuJIT
+  DefaultJob : .NET 5.0.13 (5.0.1321.56516), X64 RyuJIT
+
+
+```
+| Method |     Mean |     Error |    StdDev |   Median |
+|------- |---------:|----------:|----------:|---------:|
+|  Bench | 1.303 ms | 0.1390 ms | 0.3921 ms | 1.189 ms |
